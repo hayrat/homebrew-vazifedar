4f21b6f7d33d48801a433d96865006fae39f5f47
Ömer Faruk Taşdemir <omer.fruk3547@gmail.com>
2026-09-18 10:16:29 +0300

    vazifedar 0.0.19

--- Casks/vazifedar.rb
+++ Casks/vazifedar.rb
@@ -2,8 +2,8 @@
 # the next release overwrites this file. Version and checksum are filled in
 # from the DMG that was just notarized and published.
 cask "vazifedar" do
-  version "0.0.18"
-  sha256 "8d5337272eaab300de3eb09074daa921517684305790059d1ed8a3641f7d97a9"
+  version "0.0.19"
+  sha256 "1d625d1b75ded86c51dcc3cf96415ed0da20fa17c76d93dece75adda1317db79"
 
   url "https://github.com/hayrat/homebrew-vazifedar/releases/download/macos-#{version}/Vazifedar.dmg",
       verified: "github.com/hayrat/homebrew-vazifedar/"
